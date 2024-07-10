0339afa5aeaf0d07d19a81e74cd24cb08cc9dd44
aashishdhanani <aashishd2004@gmail.com>
2024-07-10 00:46:51 -0500

    Initial Commit

---
+++ AIFashion/AIFashionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIFashionApp.swift
+//  AIFashion
+//
+//  Created by Aashish Dhanani on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AIFashionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
